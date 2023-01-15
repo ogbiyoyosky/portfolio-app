5ccf4412489be52c92a5b48403fe81c5d32f9a22
Emmanuel Ogbiyoyo <ogbiyoyosky@yahoo.com>
2023-01-15 14:00:26 +0000

    Add routing to website

--- script.js
+++ script.js
@@ -3,41 +3,41 @@ document.addEventListener("DOMContentLoaded", function() {
     document.body.classList.add('remove-splash');
   });
 
-document
-    .querySelectorAll('.navigation ul li a')
-    .forEach(trigger => {
-        trigger.onclick = function(e) {
-            e.preventDefault();
-            let hash = this.getAttribute('href');
-            let target = document.querySelector(hash);
-            let headerOffset = 100;
-            let elementPosition = target.offsetTop;
-            let offsetPosition = elementPosition - headerOffset;
+// document
+//     .querySelectorAll('.navigation ul li a')
+//     .forEach(trigger => {
+//         trigger.onclick = function(e) {
+//             e.preventDefault();
+//             let hash = this.getAttribute('href');
+//             let target = document.querySelector(hash);
+//             let headerOffset = 100;
+//             let elementPosition = target.offsetTop;
+//             let offsetPosition = elementPosition - headerOffset;
 
-            window.scrollTo({
-                top: offsetPosition,
-                behavior: "smooth"
-            });
-        };
-    });
+//             window.scrollTo({
+//                 top: offsetPosition,
+//                 behavior: "smooth"
+//             });
+//         };
+//     });
 
-    document
-    .querySelectorAll('.footer ul li a')
-    .forEach(trigger => {
-        trigger.onclick = function(e) {
-            e.preventDefault();
-            let hash = this.getAttribute('href');
-            let target = document.querySelector(hash);
-            let headerOffset = 100;
-            let elementPosition = target.offsetTop;
-            let offsetPosition = elementPosition - headerOffset;
+//     document
+//     .querySelectorAll('.footer ul li a')
+//     .forEach(trigger => {
+//         trigger.onclick = function(e) {
+//             e.preventDefault();
+//             let hash = this.getAttribute('href');
+//             let target = document.querySelector(hash);
+//             let headerOffset = 100;
+//             let elementPosition = target.offsetTop;
+//             let offsetPosition = elementPosition - headerOffset;
 
-            window.scrollTo({
-                top: offsetPosition,
-                behavior: "smooth"
-            });
-        };
-    });
+//             window.scrollTo({
+//                 top: offsetPosition,
+//                 behavior: "smooth"
+//             });
+//         };
+//     });
 
     const nav = document.querySelector('#navigation');
     const navTop = nav.offsetTop;
